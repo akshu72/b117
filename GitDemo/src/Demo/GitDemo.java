8c9dcb35ac574df7b17970ace8befea55fc48536
HP <HP@LAPTOP-QDRHP3ET>
2020-09-05 15:03:16 +0530

    commit by Aksh 5-sept

---
+++ GitDemo/src/Demo/GitDemo.java
@@ -0,0 +1,13 @@
+package Demo;
+
+public class GitDemo 
+{
+	public void Sum() 
+	{
+		 System.out.println("Add");
+	}
+  public static void main(String[] args) 
+  {
+	System.out.println("Sum Method");
+}
+}
