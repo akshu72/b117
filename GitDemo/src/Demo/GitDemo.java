47a5cf908ed2d32629ee96577e8bf4b018baf928
akshu72 <70796405+akshu72@users.noreply.github.com>
2020-09-07 12:02:16 +0530

    Update GitDemo.java
    by ak

--- GitDemo/src/Demo/GitDemo.java
+++ GitDemo/src/Demo/GitDemo.java
@@ -4,7 +4,12 @@ public class GitDemo
 {
 	public void Sum() 
 	{
-		 System.out.println("Add");
+		 System.out.println("sum");
+	}
+	
+	public void Sub() 
+	{
+		 System.out.println("Sub);
 	}
   public static void main(String[] args) 
   {
