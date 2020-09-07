914ff839cf03f768b659030c6e44587045ced5ea
akshu72 <70796405+akshu72@users.noreply.github.com>
2020-09-07 14:31:23 +0530

    by aksh

--- GitDemo/src/Demo/GitDemo.java
+++ GitDemo/src/Demo/GitDemo.java
@@ -9,12 +9,9 @@ public class GitDemo
 	
 	public void Sub() 
 	{
-		 System.out.println("Sub);
+		 System.out.println("Sub");
 	}
-	public void mul() 
-	{
-		 System.out.println("mul);
-	}
+	
   public static void main(String[] args) 
   {
 	System.out.println("Sum Method");
