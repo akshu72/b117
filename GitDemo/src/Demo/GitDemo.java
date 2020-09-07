1b47b68d20283ba24fcb82fe011cc042b1d23a86
akshu72 <70796405+akshu72@users.noreply.github.com>
2020-09-07 12:22:38 +0530

    Update GitDemo.java
    by git account

--- GitDemo/src/Demo/GitDemo.java
+++ GitDemo/src/Demo/GitDemo.java
@@ -11,6 +11,10 @@ public class GitDemo
 	{
 		 System.out.println("Sub);
 	}
+	public void mul() 
+	{
+		 System.out.println("mul);
+	}
   public static void main(String[] args) 
   {
 	System.out.println("Sum Method");
